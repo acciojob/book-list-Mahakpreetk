fc06b70745f2c2087d970d56bb96699bd181f5a3
Mahakpreet Kaur <109897983+Mahakpreetk@users.noreply.github.com>
2023-08-02 18:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+function addBook(){
+	const title= document.getElementById('title').value;
+	const author=document.getElementById('author').value;
+	const isbn = document.getElementById('isbn').value;
+
+	if(title && author && isbn){
+		const bookList = document.getElementById('book-list');
+		const newRow=document.createElement('tr');
+		newRow.innerHTML = `
+          <td>${title}</td>
+          <td>${author}</td>
+          <td>${isbn}</td>
+          <td><button class="delete" onclick="deleteBook(this)">Delete</button></td>
+        `;
+		bookList.appendChild(newRow);
+
+		//Clear input fields after adding a new row
+		document.getElementById('title').value='';
+		document.getElementById('author').value='';
+		document.getElementById('isbn').value='';
+	}
+	else{
+		alert('Please fill in details');
+	}
+		
+	}
+
+function deleteBook(button){
+	const row=button.parentNode.parentNode;
+	row.remove;
+}
+
+document.getElementById('submit').addEventListener('click', addBook);
+}
